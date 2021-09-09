12f19981d09a5cffdfc907eeaabf0a06705b4452
vellerw <89977810+vellerw@users.noreply.github.com>
2021-09-09 23:01:47 +0300

    генератор 2.0.

---
+++ 0909h_2.py
@@ -0,0 +1,5 @@
+import random
+my_list = ['Sergey' , 'Julia' , 'Egor' , 'Igor' , 'Evelin' , 'German' , 'Bagrat' , 'Regina' , 'Bogdan' , 'Leonid' , 'Xenia' , 'Leonard' , 'Ivan' , 'Elena']
+random.shuffle(my_list)
+i = my_list.pop()
+print(i)
